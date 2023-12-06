a4b075951393fc60e303e7b84bd0db17efe2b955
Oli Hock <oliver.hock@gmail.com>
2023-12-06 18:50:49 +0100

    Refactor weather.js to use async/await syntax and remove unnecessary code

--- weather-exercise/weather.js
+++ weather-exercise/weather.js
@@ -1,20 +1,16 @@
 async function getWeatherData(cityName) {
-    try {
+
         // noinspection JSUnresolvedReference
         const apiKey = process.env.OPEN_WEATHER_MAP_API_KEY;
         const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
 
-        const response = await fetch(apiUrl);
-        const data = await response.json();
+        const responsePromise= await fetch(apiUrl);
+        const data = await responsePromise.json();
 
         // noinspection JSUnresolvedReference
-        return data.main.temp;
-    } catch (error) {
-        console.error("Error fetching weather data:", error);
-    }
+        console.log('Temperature in Hamburg is ' + data.main.temp + ' degrees Celsius.');
 }
 
 // Test the function
-getWeatherData('Hamburg')
-    .then(temperature => console.log('Temperature in Hamburg is ' + temperature + ' degrees Celsius.'))
-    .catch(error => console.error('Error fetching weather data:', error));
+// noinspection JSIgnoredPromiseFromCall
+getWeatherData('Hamburg');
